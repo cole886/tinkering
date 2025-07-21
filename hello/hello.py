b31a4b1beafdcfa8913929395a2ce70a5d749ea0
colet <cole.talerico@gmail.com>
2025-07-21 19:54:55 -0400

    playing around

---
+++ hello/hello.py
@@ -0,0 +1,23 @@
+name="bob"
+name1="cole"
+
+    
+    
+    
+    
+name="mom"
+name1="chips"
+print(f"{name}  is eating {name1}!") 
+
+# string
+name="Cole"
+
+# int
+
+a=10
+b=100
+
+
+print(f"Hello, World {name}!")
+
+print(f"Sum of {a} and {b} is {a * b}")
