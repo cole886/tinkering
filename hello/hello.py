830a8984f38786cfe81682e04e9941f231636995
colet <cole.talerico@gmail.com>
2025-07-21 20:29:03 -0400

    loops

--- hello/hello.py
+++ hello/hello.py
@@ -9,15 +9,19 @@ name="mom"
 name1="chips"
 print(f"{name}  is eating {name1}!") 
 
-# string
-name="Cole"
-
-# int
+for i in range(5):
+    print(f"Iteration {i}") 
+    
+i= 4
+while ( i < 5):
+    print(f"While loop iteration {i}")
+    i += 1
 
 a=10
 b=100
 
 
+name = input("Enter your name: ")
 print(f"Hello, World {name}!")
 
 print(f"Sum of {a} and {b} is {a * b}")
