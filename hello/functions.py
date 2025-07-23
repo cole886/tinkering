d7f8369009f2f12bdf57e59bb46f6b0e3c3b58a7
colet <cole.talerico@gmail.com>
2025-07-23 07:51:49 -0400

    function

--- hello/functions.py
+++ hello/functions.py
@@ -4,15 +4,12 @@ def f(qty,item,price):
     item="banana"
     price=1.74
     print( f' {qty} {item} cost ${price}')
-def add(a ,b):
+    
+    
+def add(a,b):
+    return a+b
+
 a=10
 b=20
-print (f'sum of a={a} and b={b} = {a + b}')
-
-
-
-def add (a,b):
- a=10
- b=20
- print(f"sum of a={a} + b={b} is  {add(a,b)}")
+print(f"sum of a={a} + b={b} is  {add(a,b)}")
 
